3d5e561175296b3bb43e456803a125a0bcdf11e0
Helena <helenrike@telefonica.net>
2023-05-05 10:33:06 +0200

    Updated readme & python script

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
+# My Git Project 
+
+> My innocent little change
 
 ## I´m  in the feature branch
+
 
 > Another code change again
 
--- my_file.py
+++ my_file.py
@@ -5,9 +5,7 @@ print("Hi from another dev")
 def greeting () :
     print("My local change")
 
-def greeting () :
-    print("Hi there")
 
 def greeting () :
-    print("Updated python fuction for merge conflict demo")
+    print("Updated python funcion for merge conflict demo")
 
